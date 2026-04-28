96984ed5946ab7a4c670aef55bc7a5bc24773841
jaykokil <jaykokil1999@gmail.com>
2026-04-28 12:51:09 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import BarcodeScaleConnector from "./components/BarcodeScaleConnector";
+
+export default function App() {
+  return (
+    <main className="page">
+      <BarcodeScaleConnector />
+    </main>
+  );
+}
